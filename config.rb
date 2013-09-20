f337e782cbd322e4d21532f0d76317bf0a104496
Stephane COUE <steph.coue@gmail.com>
2013-09-20 13:03:43 +0200

    Styled .say-hello section

--- config.rb
+++ config.rb
@@ -1,6 +1,5 @@
-require 'susy'
 # Require any additional compass plugins here.
-
+require 'susy'
 
 # Set this to the root of your project when deployed:
 http_path = "/"
@@ -12,12 +11,12 @@ javascripts_dir = "js"
 
 # You can select your preferred output style here (can be overridden via the command line):
 # output_style = :expanded or :nested or :compact or :compressed
-output_style = :expanded
+output_style =  :compact
 
 # To enable relative paths to assets via compass helper functions. Uncomment:
 relative_assets = true
 
 # To disable debugging comments that display the original location of your selectors. Uncomment:
-# line_comments = false
+line_comments = false
 
 preferred_syntax = :sass
